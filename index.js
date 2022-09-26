e1fd42aa8493f5072767f2febf19956cdf5de853
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-09-25 22:36:12 -0500

    feat: Set-up & Connect MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // -- http://localhost:3000/ -- //
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -11,8 +12,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
+/* Connect MongoDB */
+let mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
