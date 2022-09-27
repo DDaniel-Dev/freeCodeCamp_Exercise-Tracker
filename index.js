9bda80c6385ed255b21a1b471e830931fdf2cde0
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-09-26 23:54:53 -0500

    feat: GET request to return ALL tested data with username and _id

--- index.js
+++ index.js
@@ -40,20 +40,29 @@ app.get("/mongo-status", (req, res) => {
             3: disconnecting; */
 });
 
-
-// -- POST request to /api/users with form data username to create new users; includes _id -- //
-app.post("/api/users", (req, res) => {
-  const username = req.body.username
-  const user = new User({ username })
-  user.save((err, data) => {
-    if (err) {
-      res.json({ error: err})
-    } else {
-      res.json(data)
-    };
+app.route("/api/users")
+  // -- POST request to /api/users with form data username to create new users; includes _id -- //
+  .post((req, res) => {
+    const username = req.body.username
+    const user = new User({ username })
+    user.save((err, data) => {
+      if (err) {
+        res.json({ error: err})
+      } else {
+        res.json(data)
+      };
+    })
   })
-});
-
+  // -- GET request to /api/users to return an array with username and _id -- //
+  .get((req, res) => {
+    User.find((err, data) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.json(data)
+      }
+    })
+  })
 
 
 
