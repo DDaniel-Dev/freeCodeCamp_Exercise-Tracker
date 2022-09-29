f783c46e74575caa0d78442cd26d6223f8e7ffcd
DDaniel-Dev <ddaniel.atx@gmail.com>
2022-09-28 19:08:30 -0500

    feat: Re-setup Middleware

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ mongoose.connect(mongodb_URL, {
 );
 
 
+// Middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
